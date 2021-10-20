cd8b760d8793566b785afe3efc3f0ac0dd9cd216
Trey Hayden <treyhay@gmail.com>
2021-10-20 12:53:53 +0000

    creating database doc

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ let begin = require('@architect/functions')
 
 exports.handler = async function http(req) {
   
-  let session = await begin.http.session.read(req)
-  
-  let account = await data.get({
-    table: 'accounts',
-    key: session.accountID
-  })
+  let table = 'greetings'
+  let key = 'Japanese'
+  let greeting = `Kon'nichiwa`
 
+  await data.set({table, key, greeting})
+
+  let doc = await data.get({table, key})
+  
+  console.log('doc', doc)
+  
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     statusCode: 200,
-    body: {
-      status: "good",
-      thing: `Hello ${account.name}!`,
-      num: 1
-    }
+    body:  `Hello ${table}!`
   }
 }
 
