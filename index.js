4f932b990ca08e08a75947dd3c27ccff192f2d97
catchblock <treyhay@gmail.com>
2021-10-20 10:07:36 -0400

    @$

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 let data = require('@begin/data')
-let begin = require('@architect/functions') 
+let begin = require('@architect/functions')
 
 exports.handler = async function http(req) {
-  
+
   let table = 'greetings'
   let key = 'Japanese'
   let greeting = `Kon'nichiwa`
 
-  await data.set({table, key, greeting})
+  try {
+    await data.set({ table, key, greeting })
 
-  let doc = await data.get({table, key})
-  
-  console.log('doc', doc)
-  
+    let doc = await data.get({ table, key })
+
+    console.log('doc', doc)
+  } catch (error) {
+    console.error(error)
+  }
+
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     statusCode: 200,
-    body:  `Hello ${table}!`
+    body: JSON.stringify({ thing: `Hello ${table}!` })
   }
 }
-
-
-// Reads & writes session data
-
-exports.handler = async function http(request) {
-  
-}
-// Other example responses
-
-/* Forward requester to a new path
-exports.handler = async function http (req) {
-  return {
-    statusCode: 302,
-    headers: {'location': '/about'}
-  }
-}
-*/
 
 /* Respond with successful resource creation, CORS enabled
 let arc = require('@architect/functions')
