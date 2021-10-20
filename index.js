015ecb2b369dcaa74079c07825cdde5e0664656d
Trey Hayden <treyhay@gmail.com>
2021-10-20 12:45:02 +0000

    returning json

--- index.js
+++ index.js
@@ -24,11 +24,15 @@ exports.handler = async function http(req) {
 
   return {
     headers: {
-      'content-type': 'text/html; charset=utf8',
+      'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     statusCode: 200,
-    body: html
+    body: {
+      status: "good",
+      thing: "here",
+      num: 1
+    }
   }
 }
 
